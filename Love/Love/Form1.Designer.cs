d462572d513d606e986405d74a622b077b956023
Hannes Alveheim <haalv0223@skynet.halmstad.se>
2020-02-10 14:55:49 +0100

    Nah

---
+++ Love/Love/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Love
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pcbx1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pcbx1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pcbx1
+            // 
+            this.pcbx1.Location = new System.Drawing.Point(12, 12);
+            this.pcbx1.Name = "pcbx1";
+            this.pcbx1.Size = new System.Drawing.Size(1689, 1070);
+            this.pcbx1.TabIndex = 0;
+            this.pcbx1.TabStop = false;
+            this.pcbx1.Click += new System.EventHandler(this.Pcbx1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1684, 1031);
+            this.Controls.Add(this.pcbx1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pcbx1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pcbx1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Love/Love/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Love
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+
+        Random random = new Random();
+
+         
+        void love()
+        {
+            pcbx1.Image = Image.FromFile("C:\\Users\\haalv0223\\Pictures\\Hearts" + "\\" + random.Next(1, 11).ToString() + ".png");
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            love();
+        }
+
+        private void Pcbx3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Pcbx1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
