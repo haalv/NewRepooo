3bb42c0edc2dbb1c5567735a24e3d0cc6267b45f
Hannes Alveheim <haalv0223@skynet.halmstad.se>
2020-02-11 10:51:00 +0100

    yeet

--- Love/Love/Form1.Designer.cs
+++ Love/Love/Form1.Designer.cs
@@ -31,14 +31,16 @@
             this.components = new System.ComponentModel.Container();
             this.pcbx1 = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pcbx1)).BeginInit();
             this.SuspendLayout();
             // 
             // pcbx1
             // 
-            this.pcbx1.Location = new System.Drawing.Point(12, 12);
+            this.pcbx1.Location = new System.Drawing.Point(70, 24);
             this.pcbx1.Name = "pcbx1";
-            this.pcbx1.Size = new System.Drawing.Size(1689, 1070);
+            this.pcbx1.Size = new System.Drawing.Size(929, 644);
+            this.pcbx1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pcbx1.TabIndex = 0;
             this.pcbx1.TabStop = false;
             this.pcbx1.Click += new System.EventHandler(this.Pcbx1_Click);
@@ -49,17 +51,29 @@
             this.timer1.Interval = 1000;
             this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Papyrus", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(690, 755);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(809, 100);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "KÄRLEKSVECKAN";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1684, 1031);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.pcbx1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pcbx1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -67,6 +81,7 @@
 
         private System.Windows.Forms.PictureBox pcbx1;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
