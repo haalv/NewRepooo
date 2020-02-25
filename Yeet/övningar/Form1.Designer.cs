b60ea33894a8d63708dc11f9ed1a9bf8ec19a96b
Hannes Alveheim <haalv0223@skynet.halmstad.se>
2020-02-25 11:57:35 +0100

    yes

---
+++ Yeet/övningar/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace övningar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnKasta = new System.Windows.Forms.Button();
+            this.tbxShow = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnKasta
+            // 
+            this.btnKasta.Location = new System.Drawing.Point(263, 89);
+            this.btnKasta.Name = "btnKasta";
+            this.btnKasta.Size = new System.Drawing.Size(127, 23);
+            this.btnKasta.TabIndex = 0;
+            this.btnKasta.Text = "kasta tärningarna";
+            this.btnKasta.UseVisualStyleBackColor = true;
+            this.btnKasta.Click += new System.EventHandler(this.BtnKasta_Click);
+            // 
+            // tbxShow
+            // 
+            this.tbxShow.Location = new System.Drawing.Point(263, 119);
+            this.tbxShow.Multiline = true;
+            this.tbxShow.Name = "tbxShow";
+            this.tbxShow.ReadOnly = true;
+            this.tbxShow.Size = new System.Drawing.Size(127, 147);
+            this.tbxShow.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1023, 664);
+            this.Controls.Add(this.tbxShow);
+            this.Controls.Add(this.btnKasta);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnKasta;
+        private System.Windows.Forms.TextBox tbxShow;
+    }
+}
+
---
+++ Yeet/övningar/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace övningar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random rndm = new Random();
+
+        int[] kast = new int[6];
+
+        int kastVärde;
+
+        string[] resultatLista = new string[7];
+
+        int summa;
+
+        void kastaTärningar()
+        {
+            summa = 0;
+            for(int i = 0; i < 6; i++)
+            {
+                kastVärde = rndm.Next(1, 7);
+                kast[i] = kastVärde;
+                summa += kastVärde;
+            }
+        }
+
+        void resultat()
+        {
+            tbxShow.Text = "";
+            for (int i = 0; i < 6; i++)
+            {
+                resultatLista[i] = "Tärning " + (i + 1).ToString() + ":    " + kast[i].ToString();
+                tbxShow.Text += resultatLista[i] + "\r\n";
+            }
+            resultatLista[6] = "\r\n" + "Antal prickar: " + summa.ToString();
+            tbxShow.Text += resultatLista[6];
+        }
+
+        private void BtnKasta_Click(object sender, EventArgs e)
+        {
+            
+            kastaTärningar();
+            resultat();
+        }
+    }
+}
