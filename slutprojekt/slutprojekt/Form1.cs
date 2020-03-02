c96acc8c0e6c5a9f8573705e32585b8b3c9cbe2a
Hannes Alveheim <haalv0223@skynet.halmstad.se>
2020-03-02 14:32:38 +0100

    yeet

--- slutprojekt/slutprojekt/Form1.cs
+++ slutprojekt/slutprojekt/Form1.cs
@@ -12,9 +12,41 @@ namespace slutprojekt
 {
     public partial class Form1 : Form
     {
+        //Skapa en lista med knappar
+        List<Button> knappLista = new List<Button>();
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //lägg till en knapp i knapplistan
+            knappLista.Add(new Button());
+
+            //Ge egenskaper till knapp i knapplistan
+            knappLista.ElementAt(0).Location = new System.Drawing.Point(50, 50);
+            knappLista.ElementAt(0).Size = new System.Drawing.Size(20, 20);
+            knappLista.ElementAt(0).UseVisualStyleBackColor = true;
+            knappLista.ElementAt(0).TabIndex = 0;
+            knappLista.ElementAt(0).Text = "Knapp 1";
+
+            this.Controls.Add(this.knappLista.ElementAt(0));
+
+            knappLista.ElementAt(0).Click += new System.EventHandler(this.click);
+        }
+
+        private void click(object sender, EventArgs e)
+        {
+            Button b = sender as Button;
+            MessageBox.Show("Hej på mig står det" + b.Text);
         }
     }
 }
