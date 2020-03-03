e58972f6ba82bf4645fe6a62d5c772441fc9ae31
Hannes Alveheim <haalv0223@skynet.halmstad.se>
2020-03-03 12:01:36 +0100

    yes

--- slutprojekt/slutprojekt/Form1.Designer.cs
+++ slutprojekt/slutprojekt/Form1.Designer.cs
@@ -28,25 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnEasy = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // btnEasy
             // 
-            this.button1.Location = new System.Drawing.Point(23, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            this.btnEasy.BackColor = System.Drawing.SystemColors.Control;
+            this.btnEasy.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnEasy.Location = new System.Drawing.Point(23, 12);
+            this.btnEasy.Name = "btnEasy";
+            this.btnEasy.Size = new System.Drawing.Size(75, 23);
+            this.btnEasy.TabIndex = 0;
+            this.btnEasy.Text = "Easy";
+            this.btnEasy.UseVisualStyleBackColor = false;
+            this.btnEasy.Click += new System.EventHandler(this.Button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnEasy);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -56,7 +58,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnEasy;
     }
 }
 
--- slutprojekt/slutprojekt/Form1.cs
+++ slutprojekt/slutprojekt/Form1.cs
@@ -15,38 +15,75 @@ namespace slutprojekt
         //Skapa en lista med knappar
         List<Button> knappLista = new List<Button>();
 
+        int btnNumber;
+
+        int mode;
 
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void Button1_Click(object sender, EventArgs e)
+        int räknare = 0;
+
+        int yVärde = 50;
+
+        int easy = 25;
+
+        int xVärde = 50;
+
+        int nyRad = 1;
+
+        void skapaKnappar()
         {
+
+            for (int i = 0; i < mode; i++)
+            {
+                //lägg till en knapp i knapplistan
+                knappLista.Add(new Button());
+
+
+
+                //Ge egenskaper till knapp i knapplistan
+                knappLista.ElementAt(i).Location = new System.Drawing.Point((xVärde + (i*50)), yVärde);
+                knappLista.ElementAt(i).Size = new System.Drawing.Size(20, 20);
+                knappLista.ElementAt(i).UseVisualStyleBackColor = true;
+                knappLista.ElementAt(i).TabIndex = 0;
+
+                //Lägg till knappen i Formen
+                this.Controls.Add(this.knappLista.ElementAt(i));
+
+                //händelse då knappen trycks
+                knappLista.ElementAt(i).Click += new System.EventHandler(this.click);
+
+                knappLista.ElementAt(i).Name = i.ToString();
+                if (nyRad % 5 == 0)
+                {
+                    yVärde += 50;
+
+                    xVärde -= 250;
+                }
+                nyRad++;
+            }
 
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            //lägg till en knapp i knapplistan
-            knappLista.Add(new Button());
+            
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            mode = easy;
 
-            //Ge egenskaper till knapp i knapplistan
-            knappLista.ElementAt(0).Location = new System.Drawing.Point(50, 50);
-            knappLista.ElementAt(0).Size = new System.Drawing.Size(20, 20);
-            knappLista.ElementAt(0).UseVisualStyleBackColor = true;
-            knappLista.ElementAt(0).TabIndex = 0;
-            knappLista.ElementAt(0).Text = "Knapp 1";
-
-            this.Controls.Add(this.knappLista.ElementAt(0));
-
-            knappLista.ElementAt(0).Click += new System.EventHandler(this.click);
+            skapaKnappar();
         }
 
         private void click(object sender, EventArgs e)
         {
             Button b = sender as Button;
-            MessageBox.Show("Hej på mig står det" + b.Text);
+            b.BackColor = Color.FromArgb(255, 0, 0);
+            //knappLista.ElementAt(0).BackColor = Color.FromArgb(255,0,0);
         }
     }
 }
