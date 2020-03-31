120ead3d9a1ccef7f072c357bc2e1e374338c9e0
Hanoos <haalv0223@skynet.halmstad.se>
2020-03-31 12:08:40 +0200

    yeeet

---
+++ slutprojekt/knappSpel/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace knappSpel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.timerKlick = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(26, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Starta";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // timerKlick
+            // 
+            this.timerKlick.Interval = 400;
+            this.timerKlick.Tick += new System.EventHandler(this.timerKlick_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Timer timerKlick;
+    }
+}
+
---
+++ slutprojekt/knappSpel/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace knappSpel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<Button> knappLista = new List<Button>();
+
+        bool nyStart = true;
+
+        int xVärde = 100;
+
+        int yVärde = 100;
+
+        bool fårKlicka = true;
+
+        Button b;
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if(nyStart == true)
+            {
+                string tag;
+
+                for (int i = 0; i < 25; i++)
+                {
+                    knappLista.Add(new Button());
+
+                    //Ge egenskaper till knapp i knapplistan
+                    knappLista.ElementAt(i).Location = new System.Drawing.Point((xVärde + (i * 100)), yVärde);
+                    knappLista.ElementAt(i).Size = new System.Drawing.Size(50, 50);
+                    knappLista.ElementAt(i).UseVisualStyleBackColor = true;
+                    knappLista.ElementAt(i).TabIndex = 0;
+                    knappLista.ElementAt(i).BackColor = Color.FromArgb(255, 255, 255);
+                    knappLista.ElementAt(i).Tag = i;
+
+                    //Lägg till knappen i Formen
+                    this.Controls.Add(this.knappLista.ElementAt(i));
+
+                    //händelse då knappen trycks
+                    knappLista.ElementAt(i).Click += new System.EventHandler(this.klickad);
+
+                    if ((i + 1) % 5 == 0)
+                    {
+                        xVärde += -500;
+                        yVärde += 100;
+                    }
+                }
+            }
+        }
+        void klickad(object sender, EventArgs e)
+        {
+            if (fårKlicka == true)
+            {
+                fårKlicka = false;
+                b = sender as Button;
+                b.BackColor = Color.FromArgb(255, 0, 0);
+                timerKlick.Enabled = true;
+            }
+        }
+
+        private void timerKlick_Tick(object sender, EventArgs e)
+        {
+            b.BackColor = Color.FromArgb(255, 255, 255);
+            fårKlicka = true;
+        }
+    }
+ 
+}
