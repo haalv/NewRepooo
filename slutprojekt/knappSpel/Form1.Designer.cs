28b6fa8162febc7307136e483b7ea9cc0d3dd0ab
Hanoos <haalv0223@skynet.halmstad.se>
2020-04-12 19:26:04 +0200

    Suck it bitch

--- slutprojekt/knappSpel/Form1.Designer.cs
+++ slutprojekt/knappSpel/Form1.Designer.cs
@@ -31,6 +31,8 @@
             this.components = new System.ComponentModel.Container();
             this.btnStart = new System.Windows.Forms.Button();
             this.timerKlick = new System.Windows.Forms.Timer(this.components);
+            this.simulationTimer = new System.Windows.Forms.Timer(this.components);
+            this.lblNivå = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnStart
@@ -45,18 +47,34 @@
             // 
             // timerKlick
             // 
-            this.timerKlick.Interval = 400;
+            this.timerKlick.Interval = 300;
             this.timerKlick.Tick += new System.EventHandler(this.timerKlick_Tick);
+            // 
+            // simulationTimer
+            // 
+            this.simulationTimer.Interval = 600;
+            this.simulationTimer.Tick += new System.EventHandler(this.simulationTimer_Tick);
+            // 
+            // lblNivå
+            // 
+            this.lblNivå.AutoSize = true;
+            this.lblNivå.Font = new System.Drawing.Font("Modern No. 20", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNivå.Location = new System.Drawing.Point(139, 17);
+            this.lblNivå.Name = "lblNivå";
+            this.lblNivå.Size = new System.Drawing.Size(0, 21);
+            this.lblNivå.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblNivå);
             this.Controls.Add(this.btnStart);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -64,6 +82,8 @@
 
         private System.Windows.Forms.Button btnStart;
         private System.Windows.Forms.Timer timerKlick;
+        private System.Windows.Forms.Timer simulationTimer;
+        private System.Windows.Forms.Label lblNivå;
     }
 }
 
--- slutprojekt/knappSpel/Form1.cs
+++ slutprojekt/knappSpel/Form1.cs
@@ -21,61 +21,155 @@ namespace knappSpel
 
         bool nyStart = true;
 
+        bool startaSpel;
+
         int xVärde = 100;
 
         int yVärde = 100;
 
-        bool fårKlicka = true;
+        List<int> matchningsLista = new List<int>();
+
+        bool fårKlicka;
 
         Button b;
+
+        Button b2;
+
+        Random random = new Random();
+
+        int värde;
+
+        int räknare = 0;
+
+        int ronder = 4;
+
+        bool spela;
+
+        bool simulation;
+
+        bool restart = false;
 
         private void btnStart_Click(object sender, EventArgs e)
         {
             if(nyStart == true)
             {
-                string tag;
-
-                for (int i = 0; i < 25; i++)
+                lblNivå.Text = ("Nivå 1");
+                if(restart == false)
                 {
-                    knappLista.Add(new Button());
-
-                    //Ge egenskaper till knapp i knapplistan
-                    knappLista.ElementAt(i).Location = new System.Drawing.Point((xVärde + (i * 100)), yVärde);
-                    knappLista.ElementAt(i).Size = new System.Drawing.Size(50, 50);
-                    knappLista.ElementAt(i).UseVisualStyleBackColor = true;
-                    knappLista.ElementAt(i).TabIndex = 0;
-                    knappLista.ElementAt(i).BackColor = Color.FromArgb(255, 255, 255);
-                    knappLista.ElementAt(i).Tag = i;
-
-                    //Lägg till knappen i Formen
-                    this.Controls.Add(this.knappLista.ElementAt(i));
-
-                    //händelse då knappen trycks
-                    knappLista.ElementAt(i).Click += new System.EventHandler(this.klickad);
-
-                    if ((i + 1) % 5 == 0)
+                    for (int i = 0; i < 25; i++)
                     {
-                        xVärde += -500;
-                        yVärde += 100;
+                        knappLista.Add(new Button());
+
+                        //Ge egenskaper till knapp i knapplistan
+                        knappLista.ElementAt(i).Location = new System.Drawing.Point((xVärde + (i * 100)), yVärde);
+                        knappLista.ElementAt(i).Size = new System.Drawing.Size(50, 50);
+                        knappLista.ElementAt(i).UseVisualStyleBackColor = true;
+                        knappLista.ElementAt(i).TabIndex = 0;
+                        knappLista.ElementAt(i).BackColor = Color.FromArgb(255, 255, 255);
+                        knappLista.ElementAt(i).Tag = i;
+
+                        //Lägg till knappen i Formen
+                        this.Controls.Add(this.knappLista.ElementAt(i));
+
+                        //händelse då knappen trycks ändras
+                        knappLista.ElementAt(i).Click += new System.EventHandler(this.klickad);
+
+                        if ((i + 1) % 5 == 0)
+                        {
+                            xVärde += -500;
+                            yVärde += 100;
+                        }
                     }
+                    restart = true;
                 }
+                startaSpel = true;
+                nyStart = false;
+                btnStart.Text = "börja nivå";
+            }
+            else if(startaSpel == true)
+            {
+                simulationTimer.Enabled = true;
+                startaSpel = false;
+                simulation = true;
             }
         }
+
+        //Händelsen när en knapp trycks
         void klickad(object sender, EventArgs e)
         {
-            if (fårKlicka == true)
+            if(simulation == true)
             {
-                fårKlicka = false;
+                if (fårKlicka == true)
+                {
+                    b2 = b;
+                    fårKlicka = false;
+                    b2 = sender as Button;
+                    b2.BackColor = Color.FromArgb(255, 0, 0);
+                    timerKlick.Enabled = true;
+                }
+            }
+
+            //om spelaren klickar
+            else if(spela == true)
+            {
                 b = sender as Button;
                 b.BackColor = Color.FromArgb(255, 0, 0);
                 timerKlick.Enabled = true;
+                if(int.Parse(b.Tag.ToString()) != matchningsLista[räknare])
+                {
+                    spela = false;
+                    ronder = 4;
+                    räknare = 0;
+                    MessageBox.Show("Du förlorade precis kompis");
+                    nyStart = true;
+                    btnStart.Text = "Starta";
+                    matchningsLista.Clear();
+                }
+                else
+                {
+                    räknare++;
+                    if(räknare == ronder)
+                    {
+                        MessageBox.Show("Grattis du klarade nivå " + (ronder - 3));
+                        ronder++;
+                        räknare = 0;
+                        matchningsLista.Clear();
+                        startaSpel = true;
+                        lblNivå.Text = "Nivå " + (ronder - 3);
+                        spela = false;
+                    }
+                }
             }
         }
 
         private void timerKlick_Tick(object sender, EventArgs e)
         {
             b.BackColor = Color.FromArgb(255, 255, 255);
+
+            b2.BackColor = Color.FromArgb(255, 255, 255);
+
+            timerKlick.Enabled = false;
+        }
+
+        private void simulationTimer_Tick(object sender, EventArgs e)
+        {
+            värde = random.Next(0, 25);
+            b = knappLista.ElementAt(värde);
             fårKlicka = true;
+            b.PerformClick();
+
+            matchningsLista.Add(new int());
+            matchningsLista[räknare] = int.Parse(b.Tag.ToString());
+
+            räknare++;
+
+            if (räknare == ronder)
+            {
+                simulationTimer.Enabled = false;
+                räknare = 0;
+                spela = true;
+                simulation = false;
+            }
         }
     }
  
